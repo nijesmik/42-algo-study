31ee95a2a2e239485e0f12e3ae8c2889704570c0
Jihyun Kim <ksk208105@gmail.com>
2024-01-03 21:48:02 +0900

    feat: 컨베이어 벨트 위의 로봇

---
+++ jihykim2/20055.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+int	N, K, cnt;
+deque<pair<int, bool>>	dq;
+
+void	rotate_dq(void);
+void	move_robot(void);
+void	put_robot(void);
+
+int	main(void)
+{
+	cin >> N >> K;
+
+	int	dur;
+	for (int i = 0; i < 2 * N; i++)
+	{
+		cin >> dur;
+		dq.push_back(make_pair(dur, false));
+	}
+
+	while (K > 0)
+	{
+		cnt++;
+		rotate_dq();
+		move_robot();
+		put_robot();
+	}
+
+	cout << cnt;
+
+	return (0);
+}
+
+void	rotate_dq(void)
+{
+	deque<pair<int, bool>>	temp = {{dq.back().first, dq.back().second}};
+	dq.pop_back();
+	dq.push_front(make_pair(temp.front().first, temp.back().second));
+	dq[N - 1].second = false;			// N번째의 로봇은 무조건 내린다(= false)
+}
+
+void	move_robot(void)
+{
+	for (int i = N - 2; i >= 0; i--)
+	{
+		if (dq[i].second == true && dq[i + 1].second == false && dq[i + 1].first > 0)
+		{
+			dq[i].second = false;
+			dq[i + 1].second = true;
+			if (--(dq[i + 1].first) == 0)
+				K--;
+		}
+	}
+	dq[N - 1].second = false;			// N번째의 로봇은 무조건 내린다(= false)
+}
+
+void	put_robot(void)
+{
+	if (dq.front().first == 0)
+		return ;
+	dq.front().second = true;
+	if (--dq.front().first == 0)
+		K--;
+}
+
+/*
+4 5
+10	1	10	6
+2	8	4	3
+
+===============
+[1-1]
+2	10	1	10
+8	4	3	6
+[1-2]
+[1-3]
+1r	10	1	10
+8	4	3	6
+===============
+[2-1]
+8	1r	10	1
+4	3	6	10
+[2-2]
+8	1	9r	1
+4	3	6	10
+[2-3]
+7r	1	9r	1
+4	3	6	10
+===============
+
+
+*/
