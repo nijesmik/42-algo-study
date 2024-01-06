82c7d985ad45a25b6f5a25b1bf40be7a86953f41
Jihyun Kim <ksk208105@gmail.com>
2024-01-06 21:54:49 +0900

    fix: 파이프 옮기기 1
    if else-if 에 대한 조건 수정 및 재귀 함수 입력 값에 대한 return 조건 추가

--- jihykim2/17070.cpp
+++ jihykim2/17070.cpp
@@ -15,23 +15,9 @@ int	main(void)
 {
 	cin >> N;
 
-	for (int i = 0; i < 16; i++)
-	{
-		for (int j = 0; j < 16; j++)
-		{
-			if (i < N && j < N)
-				cin >> home[i][j];
-			else
-				home[i][j] = 1;
-		}
-	}
-
-	for (int i = 0; i < 16; i++)
-	{
-		for (int j = 0; j < 16; j++)
-			cout << home[i][j];
-		cout << endl;
-	}
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> home[i][j];
 
 	_go_next(0, 1, HOR);
 
@@ -42,36 +28,27 @@ int	main(void)
 
 static void	_go_next(int x, int y, int type)
 {
-	cout << "x: " << x << ", y: " << y << ", type: " << type << endl;
-	if (x >= N - 1 && y >= N - 1)
+	if (x == N - 1 && y == N - 1)
 	{
-		cout << "here" << endl;
 		cnt++;
 		return ;
 	}
-	if (type == HOR)
-	{
-		if (y + 1 < N - 1 && home[x][y + 1] == 0)
-			_go_next(x, y + 1, HOR);
-		else if (y + 1 < N && x + 1 < N && home[x + 1][y + 1] == 0 && home[x][y + 1] == 0 && home[x + 1][y] == 0)
-			_go_next(x + 1, y + 1, DIA);
-	}
-	else if (type == VER)
-	{
-		if (x + 1 < N - 1 && home[x + 1][y] == 0)
-			_go_next(x + 1, y, VER);
-		else if (y + 1 < N && x + 1 < N && home[x + 1][y + 1] == 0 && home[x][y + 1] == 0 && home[x + 1][y] == 0)
-			_go_next(x + 1, y + 1, DIA);
-	}
-	else
+	else if (x > N - 1 || y > N - 1 || home[x][y] == 1)
+		return ;
+
+	if (type == HOR && y + 1 < N && home[x][y + 1] == 0)
+		_go_next(x, y + 1, HOR);
+	else if (type == VER && x + 1 < N && home[x + 1][y] == 0)
+		_go_next(x + 1, y, VER);
+	else if (type == DIA)
 	{
 		if (y + 1 < N && home[x][y + 1] == 0)
 			_go_next(x, y + 1, HOR);
-		else if (x + 1 < N && home[x + 1][y] == 0)
+		if (x + 1 < N && home[x + 1][y] == 0)
 			_go_next(x + 1, y, VER);
-		else if (y + 1 < N && x + 1 < N && home[x + 1][y + 1] == 0 && home[x][y + 1] == 0 && home[x + 1][y] == 0)
-			_go_next(x + 1, y + 1, DIA);
 	}
+	if (y + 1 < N && x + 1 < N && home[x + 1][y + 1] == 0 && home[x][y + 1] == 0 && home[x + 1][y] == 0)
+		_go_next(x + 1, y + 1, DIA);
 }
 
 
