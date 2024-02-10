8cd96ec3de45977d8493f840ec62ac116930fbc4
Jihyun Kim <ksk208105@gmail.com>
2024-02-11 00:58:03 +0900

    refactor: 배열 돌리기 3

--- jihykim2/week07/16935.cpp
+++ jihykim2/week07/16935.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
 
-// typedef void (*Func)(void);
 int N, M, R, arr[100][100], tmp[100][100];
 
 static void _row_copy(int org, int cpy, bool rotate);
 static void _col_copy(int org, int cpy, bool rotate);
-static void _quart_row_copy(int start_r, int start_c, bool forward);
-static void _quart_col_copy(int start_r, int start_c, bool forward);
+static void _up_down_copy(int col_start, bool forward);
+static void _right_left_copy(int row_start, bool forward);
 static void synchronized(void);
 
 int main(void) {
@@ -31,14 +30,25 @@ int main(void) {
             for (int c = 0; c < M; c++)
                 _col_copy(c, M - 1 - c, false);
         } else if (cmd == 3) {
+            int tmp = N;
+            N = M; M = tmp;
             for (int i = 0; i < N; i++)
-                _row_copy(i, 5 - i, true);
+                _row_copy(i, M - 1 - i, true);
         } else if (cmd == 4) {
             for (int i = 0; i < N; i++)
-                _col_copy(i, 5 - i, true);
+                _col_copy(i, N - i, true);
+            int tmp = N;
+            N = M; M = tmp;
         } else if (cmd == 5) {
-            _quart_row_copy(0, 0, true);
-            // 반씩 4번... 머리 깨질 각
+            _right_left_copy(0, true);
+            _up_down_copy(M / 2, true);
+            _right_left_copy(N / 2, false);
+            _up_down_copy(0, false);
+        } else {
+            _up_down_copy(0, true);
+            _right_left_copy(N / 2, true);
+            _up_down_copy(M / 2, false);
+            _right_left_copy(0, false);
         }
         if (R != 0)
             synchronized();
@@ -57,7 +67,7 @@ static void _row_copy(int org, int cpy, bool rotate) {
             arr[org][i] = tmp[cpy][i];
     } else {
         for (int i = 0; i < M; i++)
-            arr[org][i] = tmp[N - 1 - i][cpy];
+            arr[org][i] = tmp[i][cpy];
     }
 }
 
@@ -66,25 +76,38 @@ static void _col_copy(int org, int cpy, bool rotate) {
         for (int i = 0; i < N; i++)
             arr[i][org] = tmp[i][cpy];
     } else {
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < M; i++)
             arr[i][org] = tmp[cpy][M - 1 - i];
     }
 }
 
-static void _quart_row_copy(int start_r, int start_c, bool forward) {
-    int end_r = N, end_c = M;
-    if (start_r == 0)
-        end_r /= 2;
-    if (start_c == 0)
-        end_c /= 2;
+static void _up_down_copy(int col_start, bool forward) {
+    int col_end = M;
+    if (col_start == 0)
+        col_end /= 2;
     if (forward == true) {
-        for (int r = start_r; r < end_r; r++)
-            for (int c = start_c; c < end_c; c++)
-                arr[r][c] = tmp[r][M - 1 - c];
+        for (int r = N / 2; r < N; r++)
+            for (int c = col_start; c < col_end; c++)
+                arr[r][c] = tmp[r - N / 2][c];
     } else {
-        for (int r = start_r; r < end_r; r++)
-            for (int c = start_c; c < end_c; c++)
-                arr[r][c] = tmp[N - 1 - r][c];
+        for (int r = 0; r < N / 2; r++)
+            for (int c = col_start; c < col_end; c++)
+                arr[r][c] = tmp[r + N / 2][c];
+    }
+}
+
+static void _right_left_copy(int row_start, bool forward) {
+    int row_end = N, end_c = M;
+    if (row_start == 0)
+        row_end /= 2;
+    if (forward == true) {
+        for (int r = row_start; r < row_end; r++)
+            for (int c = M / 2; c < M; c++)
+                arr[r][c] = tmp[r][c - M / 2];
+    } else {
+        for (int r = row_start; r < row_end; r++)
+            for (int c = 0; c < M / 2; c++)
+                arr[r][c] = tmp[r][c + M / 2];
     }
 }
 
@@ -94,6 +117,8 @@ static void synchronized(void) {
             tmp[r][c] = arr[r][c];
 }
 
-/**
+/** [풀이 과정]
  * 시간이 너무 오래(2시간 넘음..)걸려서 일단은 하던 곳까지 제출..합니다... 다시 할게유
+ * 1, 2, 5, 6번은 풀었으나 3, 4번은 아직도 해결을 못하겠다
+   무작정 cmd 하나 당 하나의 함수로 풀고 싶지 않았는데 결국은 지금 풀이를 보면 그거나 그거나인것 같다
 */
