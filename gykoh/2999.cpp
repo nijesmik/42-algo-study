b0eb626f88a908b4fc9431ab56bb618c91747a8d
gykoh42 <karmakoh42@naver.com>
2024-02-19 18:56:46 +0900

    feat: 비밀 이메일

---
+++ gykoh/2999.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int N, R, C;
+int main()
+{
+	string message;
+	cin >> message;
+	N = message.length();
+	for (int i = sqrt(N); i >= 1; i--) {
+		if (N % i == 0) {
+			R = i;
+			C = N / R;
+			break ;
+		}
+	}
+
+	char arr[R][C];
+	int idx = 0;
+	for (int i = 0; i < C; i++) {
+		for (int j = 0; j < R; j++) {
+			arr[j][i] = message[idx];
+			idx++;
+		}
+	}
+
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cout << arr[i][j];
+		}
+	}
+	return 0;
+}
