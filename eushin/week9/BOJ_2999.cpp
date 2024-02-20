381ec2c3406ad6ebf0e7a43bc83e8e2c1b4c9111
gpffh20 <eunbin584836@gmail.com>
2024-02-20 17:19:23 +0900

    feat: 비밀 이메일

---
+++ eushin/week9/BOJ_2999.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+string encrypted_msg;
+
+void Input()
+{
+	cin >> encrypted_msg;
+}
+
+void Solution()
+{
+	int R, C;
+	int size = encrypted_msg.length();
+	
+	for (int i = sqrt(size); i > 0; i--)
+	{
+		if (size % i == 0)
+		{
+			R = i;
+			C = size / i;
+			break;
+		}
+	}
+
+	char arr[C][R];
+	int idx = 0;
+
+	for (int i = 0; i < C; i++)
+	{
+		for (int j = 0; j < R; j++)
+		{
+			arr[i][j] = encrypted_msg[idx];
+			idx++;
+		}
+		cout << "\n";
+	}
+
+	for (int i = 0; i < R; i++)
+		for (int j = 0; j < C; j++)
+			cout << arr[j][i];
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	Input();
+	Solution();
+}
