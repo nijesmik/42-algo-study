007905481debaa80f8d1525b922502db950cb8c5
nijesmik <bkybb@duck.com>
2024-02-25 13:17:57 +0900

    refactor: 아기 상어 2

--- sejinkim/week09/아기_상어_2.java
+++ sejinkim/week09/아기_상어_2.java
@@ -3,54 +3,39 @@ import java.util.*;
 
 public class 아기_상어_2 {
     static int sizeR, sizeC, map[][], ans;
+    static List<Node> sharks;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sizeR = sc.nextInt();
         sizeC = sc.nextInt();
         map = new int[sizeR][sizeC];
+        sharks = new ArrayList<>();
         for (int i = 0; i < sizeR; i++) {
             for (int j = 0; j < sizeC; j++) {
                 map[i][j] = sc.nextInt();
+                if (map[i][j] == 1) {
+                    sharks.add(new Node(i, j));
+                }
             }
         }
         for (int i = 0; i < sizeR; i++) {
             for (int j = 0; j < sizeC; j++) {
                 if (map[i][j] == 0) {
-                    bfs(i, j);
+                    getDist(i, j);
                 }
             }
         }
         System.out.println(ans);
     }
 
-    static int[] dr = { -1, -1, -1, 0, 0, 1, 1, 1 }, dc = { -1, 0, 1, -1, 1, -1, 0, 1 };
-
-    static void bfs(int r, int c) {
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(r, c));
-        boolean[][] visited = new boolean[sizeR][sizeC];
-        visited[r][c] = true;
-        int dist = 1;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            while (size-- > 0) {
-                Node cur = q.poll();
-                for (int i = 0; i < 8; i++) {
-                    int nr = cur.r + dr[i], nc = cur.c + dc[i];
-                    if (nr < 0 || nr >= sizeR || nc < 0 || nc >= sizeC || visited[nr][nc]) {
-                        continue;
-                    }
-                    if (map[nr][nc] == 1) {
-                        ans = Math.max(ans, dist);
-                        return;
-                    }
-                    visited[nr][nc] = true;
-                    q.add(new Node(nr, nc));
-                }
-            }
-            dist++;
+    static void getDist(int r, int c) {
+        int min = Math.max(sizeR, sizeC);
+        for (Node shark : sharks) {
+            int dist = Math.max(Math.abs(shark.r - r), Math.abs(shark.c - c));
+            min = Math.min(min, dist);
         }
+        ans = Math.max(ans, min);
     }
 
     static class Node {
