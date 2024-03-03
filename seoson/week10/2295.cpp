bc751fb3b7586f0611e6591af5b8dc213973ba44
Sungho <shson1217@kw.ac.kr>
2024-03-03 15:44:43 +0900

    fix : 세 수의 합

--- seoson/week10/2295.cpp
+++ seoson/week10/2295.cpp
@@ -1,44 +1,34 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
-int N, ans;
-vector<int> vec;
+int N;
 int arr[1000];
-
-int check_ans()
-{
-	int ret = 0;
-	for (int i = 0; i < 3; i++)
-		ret += arr[vec[i]];
-	for (int i = 0; i < N; i++)
-	{
-		if (ret == arr[i])
-			return (ret);
-	}
-	return (0);
-}
-
-void dfs(int depth)
-{
-	if (depth == 3)
-	{
-		ans = max(ans, check_ans());
-		return;
-	}
-	for (int i = depth; i < N; i++)
-	{
-		vec.push_back(i);
-		dfs(depth + 1);
-		vec.pop_back();
-	}
-}
-
+vector<int> sum;
 int main()
 {
+	ios::sync_with_stdio(0);
+	cin.tie(0),cout.tie(0);
 	cin >> N;
 	for (int i = 0; i < N; i++)
 		cin >> arr[i];
-	dfs(0);
-	cout << ans;
+	sort(arr, arr + N);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = i; j < N; j++)
+			sum.push_back(arr[i] + arr[j]);
+	}
+	sort(sum.begin(), sum.end());
+	for (int i = N - 1; i > 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (binary_search(sum.begin(), sum.end(), arr[i] - arr[j]))
+			{
+				cout << arr[i];
+				return (0);
+			}
+		}
+	}
 	return (0);
 }
