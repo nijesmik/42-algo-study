c28912176685051993933ccff35232ccbaf74907
nijesmik <bkybb@duck.com>
2024-03-16 21:12:41 +0900

    feat: Java vs C++

---
+++ sejinkim/week12/Java_vs_C.java
@@ -0,0 +1,59 @@
+package week12;
+import java.util.Scanner;
+
+public class Java_vs_C {
+	static StringBuilder sb;
+	static char[] words;
+	static int n;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+
+		words = input.toCharArray();
+		n = words.length;
+
+		String ans = convert(input.contains("_"));
+		System.out.println(ans);
+	}
+
+	static String convert(boolean isCpp) {
+		if (!Character.isLowerCase(words[0]) || words[n - 1] == '_') {
+			return "Error!";
+		}
+		sb = new StringBuilder();
+		if (isCpp) {
+			return toJava();
+		}
+		return toCpp();
+	}
+
+	static String toCpp() {
+		for (char c : words) {
+			if (c < 'a') {
+				sb.append('_');
+				c = Character.toLowerCase(c);
+			}
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+
+	static String toJava() {
+		int i = 0;
+		while (i < n) {
+			char c = words[i++];
+			if (c >= 'A' && c <= 'Z') {
+				return "Error!";
+			}
+			if (c == '_') {
+				if (!Character.isLowerCase(words[i])) {
+					return "Error!";
+				}
+				c = Character.toUpperCase(words[i++]);
+			}
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+}
