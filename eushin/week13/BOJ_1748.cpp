a298b778efd3d3fb388db2cece9111a65343b8a5
gpffh20 <eunbin584836@gmail.com>
2024-03-24 22:15:17 +0900

    feat: 수 이어 쓰기 1

---
+++ eushin/week13/BOJ_1748.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int N, cnt;
+
+void Input()
+{
+	cin >> N;
+}
+
+void Solution()
+{
+	if (N < 10)
+	{
+		cout << N;
+		return;
+	}
+
+	for (int i = 1; i <= N; i *= 10)
+		cnt += N - i + 1;
+
+	cout << cnt;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	Input();
+	Solution();
+}
